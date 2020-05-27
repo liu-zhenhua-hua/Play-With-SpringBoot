6a75a4244ba78c2c95b44a7f093d8215d7b76156
Liu Zhenhua <join_github_repo@126.com>
2020-05-27 17:09:59 +0800

    Updating README.md for spring-boot-learn-02

---
+++ spring-boot-learn-02/src/main/java/org/cherry/learn/bean/Person.java
@@ -0,0 +1,10 @@
+package org.cherry.learn.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Created by tony on 2020/5/27.
+ */
+@ConfigurationProperties
+public class Person {
+}
