457c8f290421208ad4fe334c37a4bc0614c19e65
Liu Zhenhua <join_github_repo@126.com>
2020-06-06 21:17:20 +0800

    Adding new Controller File, edit the application.properties

--- spring-boot-learn-04/src/main/java/org/cherry/bookstore/controller/MyController.java
+++ spring-boot-learn-04/src/main/java/org/cherry/bookstore/controller/MyController.java
@@ -1,7 +1,12 @@
 package org.cherry.bookstore.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 /**
  * Created by tony on 2020/6/5.
@@ -10,8 +15,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MyController {
 
-//    @RequestMapping({"/","/index.html"})
-//    public String index(){
-//        return "index";
-//    }
+    @PostMapping(value="/user/login")
+    public String login(@RequestParam("username") String username,
+                        @RequestParam("password") String password,
+                        Map<String,Object> map){
+
+        if(!StringUtils.isEmpty(username) && "123456".equals(password)){
+            return "dashboard";
+        }else{
+            map.put("msg","Login Failed");
+            return "login";
+        }
+
+    }
+
 }
